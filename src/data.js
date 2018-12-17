a54fbf419c02c08a62b1703c2c4973efb27c6e54
John <johnsmilga@gmail.com>
2018-12-17 01:10:06 -0800

    changing variables

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
 export const storeProducts = [
   {
     id: 1,
